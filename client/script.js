7c520b8f7f012cbac64a1371f82bda0db89e4ae8
Jenn Marshall <jennyfever@gmail.com>
2023-02-21 14:31:11 -0700

    edited endpoints

--- client/script.js
+++ client/script.js
@@ -74,15 +74,15 @@ const handleSubmit = async (e) => {
 
   // fetch data from openAi
 
-  const response = await fetch('http://localhost:5000', {
-    method: 'POST',
+  const response = await fetch("https://fun-with-openai.onrender.com/", {
+    method: "POST",
     headers: {
-      'Content-Type': 'application/json'
+      "Content-Type": "application/json",
     },
     body: JSON.stringify({
-      prompt: data.get('prompt')
-    })
-  })
+      prompt: data.get("prompt"),
+    }),
+  });
   clearInterval(loadInterval);
   messageDiv.innerHTML = '';
 
